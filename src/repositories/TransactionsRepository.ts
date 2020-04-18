a1dd311deba60e5f0ac8e2511f4588f8f9a13430
Mateus Miranda <mateusm.miranda80@gmail.com>
2020-04-18 20:57:32 -0300

    Finalizando o primeiro desafio da fase 02

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,7 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
-
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +18,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let somaIncome = 0;
+    let somaOutcome = 0;
+    this.transactions.map(transaction => {
+      if (transaction.type === 'income') somaIncome += transaction.value;
+      return somaIncome;
+    });
+    this.transactions.map(transaction => {
+      if (transaction.type === 'outcome') somaOutcome += transaction.value;
+      return somaOutcome;
+    });
+    const Balance = {
+      income: somaIncome,
+      outcome: somaOutcome,
+      total: somaIncome - somaOutcome,
+    };
+    return Balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,20 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: TransactionDTO): Transaction {
+    const Balance = this.transactionsRepository.getBalance();
+    if (Balance.total - value < 0 && type === 'outcome') {
+      throw Error(
+        'Action not authorized because the value is bigger than your total',
+      );
+    }
+
+    const transactionCreated = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transactionCreated;
   }
 }
 
